8e47f4c701a7874e98d8e6c024437c1855b0bed6
raihan0101 <raihan0101@gmail.com>
2017-03-01 20:11:51 +0600

    array ds

---
+++ src/datastructure/arrays/ArraysDS.java
@@ -0,0 +1,20 @@
+package datastructure.arrays;
+
+import java.util.Scanner;
+
+/**
+ * Created by rayhan on 3/1/17.
+ */
+public class ArraysDS {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int arr[] = new int[n];
+        for(int arr_i=0; arr_i < n; arr_i++){
+            arr[arr_i] = in.nextInt();
+        }
+        for (int arr_j=n-1; arr_j>=0;arr_j--){
+            System.out.print(arr[arr_j]+" ");
+        }
+    }
+}
